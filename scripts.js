f72bc2c0ed6d8dd747635411f6a37ffee530cd45
ibrahimnneto <75408504+ibrahimnneto@users.noreply.github.com>
2022-04-04 21:08:45 -0300

    Initial files of the project
    Add: all the initial files needed for the project
    
    Currently the app can draw with the standard black color, an gray option and a 'random color' option. It also has an 'eraser', everything working.
    
    Need to add: only draw when holding the the click button. Currently draws everytime you hover the mouse over the 'board'

---
+++ scripts.js
@@ -0,0 +1,54 @@
+const container = document.getElementById('container');
+const btnSize = document.createElement('button');
+const buttonsContainer = document.querySelector('.buttons');
+let color = 'black';
+
+function createRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (let i = 0; i < (rows * cols); i += 1) {
+    const cell = document.createElement('div');
+    cell.addEventListener('mouseover', colorSquare);
+    container.appendChild(cell).className = 'grid-item';
+  }
+}
+
+function colorSquare() {
+  if ((color === 'random')) {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function clear() {
+  const cells = container.querySelectorAll('.grid-item');
+  cells.forEach((cell) => cell.remove());
+}
+
+function resize() {
+  btnSize.textContent = 'Reset';
+  btnSize.addEventListener('click', () => {
+    const user = prompt('WHAT SIZE YOU WANT YOUR GRID TO BE? LIMIT OF 100');
+    if (user === null || user < 1) {
+      clear();
+      createRows(16, 16);
+      // grayColor();
+      // rgbColors();
+      // blackColor();
+    } else if (user < 100) {
+      clear();
+      createRows(user, user);
+      // grayColor();
+      // rgbColors();
+      // blackColor();
+    }
+  });
+  buttonsContainer.appendChild(btnSize).classList.add('btn');
+}
+
+resize();
